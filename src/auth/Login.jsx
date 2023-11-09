0190e314fa1fa4618f80fe92524285d3fb72bb88
BryanG26 <bryangonzalez261999@gmail.com>
2023-11-08 20:39:43 -0600

    Login

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    const data = {
+      username: username,
+      password: password
+    };
+
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Usuario y/o contraseña incorrectos');
+        throw new Error('Error en la autenticación');
+      }
+    })
+    .then(data => {
+      if (data && data.token) {
+        const token = data.token;
+
+        console.log('Token de acceso:', token);
+
+        localStorage.setItem('token', token);
+
+        navigate('/');
+      } else {
+        console.error('Token no encontrado en la respuesta');
+      }
+    })
+    .catch(error => {
+      console.error('Error en la solicitud POST', error);
+    });
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-container">
+      <h1 className="text-center">Login</h1>
+      <div className="search">
+        <input
+          type="text"
+          className="search__input"
+          placeholder="Nombre de Usuario"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="search">
+        <input
+          type="password"
+          className="search__input"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="search">
+        <button className="search__button" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { MoviePage, RecentMoviesPage } from '../movies/pages'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { MoviePage, RecentMoviesPage } from '../movies/pages';
+import Login from '../auth/Login';
 
 export const AppRouter = () => {
   return (
     <Routes>
-      
+      <Route path="login" element={<Login />} />
       <Route path="/*" element={<RecentMoviesPage />} />
-      
-      <Route path='movies/:id' element={<MoviePage />}/>
+      <Route path="movies/:id" element={<MoviePage />}
+      />
     </Routes>
-  )
-}
+  );
+};
